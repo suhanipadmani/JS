4a23037bec33e42932857af94412af4b7df03344
suhanipadmani <suhanipadmani0903@gmail.com>
2025-06-09 15:42:59 +0000

    commit

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,19 @@
+let name = "Suhani"
+let age = 20
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
+console.log(typeof age);
